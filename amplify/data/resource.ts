04b7c10aa6d07de398b8644e78e684a6b8e47f19
joshuaT96 <joshua@solareff.co.za>
2024-11-28 10:24:50 +0200

    added VCOM BESS database schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,14 +3,15 @@ import { postConfirmation } from "../auth/post-confirmation/resource";
 
 const schema = a
   .schema({
-    UserProfile: a
-      .model({
+    UserProfile: a.model({
         email: a.string(),
         profileOwner: a.string(),
       })
       .authorization((allow) => [
         allow.ownerDefinedIn("profileOwner"),
       ]),        
+
+    /*
     Post: a.customType({
       id: a.id().required(),
       author: a.string().required(),
@@ -20,7 +21,22 @@ const schema = a
       ups: a.integer(),
       downs: a.integer(),
       version: a.integer(),
-  })
+    }),
+    */
+
+    VCOM_SMS_BESS_Data: a.customType({
+      index: a.string().required(),
+      commsLost: a.integer(),
+      commsLostCounter: a.integer(),
+      genPowerAbbreviation: a.string().required(),
+      genRunning: a.integer(),
+      key: a.string().required(),
+      plantName: a.string().required(),
+      soc: a.integer(),
+      socAbbreviation: a.string().required(),
+      socMinLevel: a.integer()
+    })
+
   })
   .authorization((allow) => [allow.resource(postConfirmation)]);
 export type Schema = ClientSchema<typeof schema>;
